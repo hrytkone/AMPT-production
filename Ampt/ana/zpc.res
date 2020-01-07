c4c9ca868e9b81456447f941afb6462c0c138900
Heidi Rytkönen <heidi.m.rytkonen@student.jyu.fi>
2020-01-07 13:00:40 +0200

    Added ROOT macro that saves AMPT output to tree (similar to toyFlow)

--- Ampt/ana/zpc.res
+++ Ampt/ana/zpc.res
@@ -1,1 +1,60 @@
   Event            1 , run            1
+     number of operations =        31806
+     number of collisions between particles =         4964
+     freezeout time=   280980.16081712389     
+     ending at the         4966 th random number
+     ending collision iff=          -1
+  Event            2 , run            1
+     number of operations =        24652
+     number of collisions between particles =         3768
+     freezeout time=   493837.58334353747     
+     ending at the         8734 th random number
+     ending collision iff=          -1
+  Event            3 , run            1
+     number of operations =        31499
+     number of collisions between particles =         4963
+     freezeout time=   109645.84237188111     
+     ending at the        13697 th random number
+     ending collision iff=           1
+  Event            4 , run            1
+     number of operations =        32574
+     number of collisions between particles =         5296
+     freezeout time=   201440.60516963009     
+     ending at the        18993 th random number
+     ending collision iff=           1
+  Event            5 , run            1
+     number of operations =        27712
+     number of collisions between particles =         3837
+     freezeout time=   32286.057674321881     
+     ending at the        22830 th random number
+     ending collision iff=          -1
+  Event            6 , run            1
+     number of operations =        22697
+     number of collisions between particles =         2924
+     freezeout time=   344904.37747303915     
+     ending at the        25754 th random number
+     ending collision iff=          -1
+  Event            7 , run            1
+     number of operations =        33297
+     number of collisions between particles =         5471
+     freezeout time=   75471.137317748333     
+     ending at the        31225 th random number
+     ending collision iff=           1
+  Event            8 , run            1
+     number of operations =        30363
+     number of collisions between particles =         5311
+     freezeout time=   54873.063397387385     
+     ending at the        36536 th random number
+     ending collision iff=          -1
+  Event            9 , run            1
+     number of operations =        32213
+     number of collisions between particles =         5219
+     freezeout time=   65083.583158535839     
+     ending at the        41755 th random number
+     ending collision iff=           1
+  Event           10 , run            1
+     number of operations =        28888
+     number of collisions between particles =         4983
+     freezeout time=   57219.936969709335     
+     ending at the        46738 th random number
+     ending collision iff=          -1
---
+++ ampt-run/AmptToTTree.C
@@ -0,0 +1,87 @@
+/**
+ * ROOT macro for saving ampt.dat to NTuple
+ */
+
+// Function from pythia code
+bool isHadron(int particleid) {
+    if (particleid <= 100 || (particleid >= 1000000 && particleid <= 9000000) || particleid >= 9900000)
+        return false;
+    if (particleid == 130 || particleid == 310)
+        return true;
+    if (particleid%10 == 0 || (particleid/10)%10 == 0 || (particleid/100)%10 == 0)
+        return false;
+    return true;
+}
+
+void AmptToTTree(int jobNumber, int eventStartId) {
+
+    TString inFileName(Form("ampt%01i.dat", jobNumber));
+    FILE *fIn = fopen(inFileName, "r");
+
+    TString outFileName(Form("ampt-output%02i.root", jobNumber));
+    TFile *fOut = new TFile(outFileName, "RECREATE");
+
+    Int_t split = 0;
+    Int_t bsize = 64000;
+    TClonesArray *particleList = new TClonesArray("TLorentzVector", 20000);
+    vector<int> *particleCharge = new vector<int>();
+    vector<int> *particlePID = new vector<int>();
+    vector<int> *particleIsHadron = new vector<int>();
+
+    TTree *tree = new TTree("tracks","Tree for track information");
+    tree->Branch("particleList", "TClonesArray", &particleList, bsize, split);
+    tree->Branch("particleCharge", "vector<int>", &particleCharge, bsize, split);
+    tree->Branch("particlePID", "vector<int>", &particlePID, bsize, split);
+    tree->Branch("particleIsHadron", "vector<int>", &particleIsHadron, bsize, split);
+
+    // Event variables
+    Int_t ncols = 0, eventid = 0, test = 0, ntracks = 0, npart1 = 0, npart2 = 0,
+           npart1_el = 0, npart1_inel = 0, npart2_el = 0, npart2_inel = 0;
+    Float_t b = 0.0, time = 0.0;
+
+    // Track variables
+    Int_t particleid = 0;
+    Float_t px = 0.0, py = 0.0, pz = 0.0, m = 0.0, x = 0.0, y = 0.0, z = 0.0, t = 0.0;
+    Double_t charge = 0.0;
+    bool ishadron = false;
+
+    TLorentzVector lvec;
+    Float_t energy = 0;
+
+    while (1) {
+
+        particleList->Clear("C");
+        particleCharge->clear();
+        particlePID->clear();
+        particleIsHadron->clear();
+
+        ncols = fscanf(fIn, "%d %d %d %f %d %d %d %d %d %d %f", &eventid, &test,
+                        &ntracks, &b, &npart1, &npart2, &npart1_el, &npart2_el,
+                        &npart1_inel, &npart2_inel, &time);
+
+        if (ncols<0) break;
+
+        for (Int_t i = 0; i < ntracks; i++) {
+            ncols = fscanf(fIn, "%d %f %f %f %f %f %f %f %f",
+                            &particleid, &px, &py, &pz, &m, &x, &y, &z, &t);
+            ishadron = isHadron(particleid);
+            if (TMath::Abs(particleid) != 42) {
+                charge = TDatabasePDG::Instance()->GetParticle(particleid)->Charge();
+                if (charge!=0) { //Only charged particles
+                    energy = TMath::Sqrt(TMath::Sqrt(px*px + py*py) + pz*pz);
+                    lvec.SetPxPyPzE(px, py, pz, energy);
+                    new ( (*particleList)[i] ) TLorentzVector( lvec );
+                    particleCharge->push_back(charge);
+                    particlePID->push_back(particleid);
+                    particleIsHadron->push_back(ishadron);
+                }
+            }
+        }
+        tree->Fill();
+    }
+
+   fclose(fIn);
+
+   fOut->Write();
+   fOut->Close();
+}
